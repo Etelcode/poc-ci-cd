cf379c03cb7fa39381d688a8889f01d825b88c81
lobbyra <jecaudal@student.42.fr>
2022-02-24 10:20:06 +0000

    trigger change

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -12,7 +12,7 @@ export class AppController {
     @Get('/:name')
     async hello(@Param('name') name: string): Promise<string> {
         // VX used to trigger a commit to test versions
-        return (await this.appService.hello(name) + ": v15");
+        return (await this.appService.hello(name) + ": v16");
     }
 
     @Post('/sum')
